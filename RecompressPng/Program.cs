c525c5964a104d0114a33b3222e4cf9fba03eea6
koturn <jeak.koutan.apple@gmail.com>
2020-12-01 18:01:36 +0900

    Initial commit

---
+++ RecompressPng/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using LibZopfliSharp;
+
+
+namespace RecompressPng
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Please specify target zip file");
+                return 1;
+            }
+
+            var srcZipFile = args[0];
+            var dstZipFile = srcZipFile + ".zopfli";
+
+            if (File.Exists(dstZipFile))
+            {
+                File.Delete(dstZipFile);
+            }
+
+            using (var srcArchive = ZipFile.OpenRead(srcZipFile))
+            using (var dstArchive = ZipFile.Open(dstZipFile, ZipArchiveMode.Update))
+            {
+                foreach (var srcEntry in srcArchive.Entries)
+                {
+                    if (!srcEntry.FullName.EndsWith(".png"))
+                    {
+                        Console.WriteLine($"Non target: {srcEntry.FullName}");
+                        continue;
+                    }
+                    Console.WriteLine($"Compress {srcEntry.FullName} ...");
+                    byte[] data;
+                    using (var ms = new MemoryStream(4 * 1024 * 1024))
+                    {
+                        using (var srcZs = srcEntry.Open())
+                        {
+                            srcZs.CopyTo(ms);
+                        }
+                        data = ms.ToArray();
+                    }
+
+                    var dstEntry = dstArchive.CreateEntry(srcEntry.FullName);
+                    using (var dstZs = dstEntry.Open())
+                    using (var zopfliStream = new ZopfliPNGStream(dstZs))
+                    {
+                        zopfliStream.Write(data, 0, data.Length);
+                    }
+                    dstEntry.LastWriteTime = srcEntry.LastWriteTime;
+
+                    Console.WriteLine($"Done");
+                }
+            }
+
+            MoveFileForce(
+                srcZipFile,
+                Path.Combine(Path.GetDirectoryName(srcZipFile), Path.GetFileNameWithoutExtension(srcZipFile) + ".old.zip"));
+            MoveFileForce(
+                dstZipFile,
+                Path.Combine(Path.GetDirectoryName(dstZipFile), Path.GetFileNameWithoutExtension(dstZipFile)));
+
+            return 0;
+        }
+
+        static void MoveFileForce(string srcFileName, string dstFileName)
+        {
+            if (File.Exists(dstFileName))
+            {
+                File.Delete(dstFileName);
+            }
+            File.Move(srcFileName, dstFileName);
+        }
+    }
+}
